9fcb1e01b2fe1b2c0bdd69b25ba7dabe7c687420
Zihao Chen <zihao.chen31@gmail.com>
2018-02-05 13:56:24 -0600

    xxx
    xxx

--- host.py
+++ host.py
@@ -2,21 +2,45 @@ import pandas as pd
 import re
 
 all_tweets = pd.read_json("gg2018.json")
-monologue  = all_tweets[all_tweets['text'].str.contains("s monologue ")]
-
+host = ""
 host_dict = {}
 
-for index, row in monologue.iterrows():
-    tweet_text = row["text"]
-    # print(tweet_text)
-    regex_match = re.search("[A-Z][a-z]* [A-Z][a-z]*", tweet_text)
-    
-    if regex_match:
-        if "The " not in regex_match.group(0):
-            if regex_match.group(0) in host_dict.keys():
-                host_dict[regex_match.group(0)] += 1
-            else:
-                host_dict[regex_match.group(0)] = 1
-            #print(regex_match.group(0))
 
-print(max(host_dict, key=host_dict.get))
+def getHost(tweet_text):
+    """TODO more phases here, not just only s monologue"""
+    if "s monologue" in tweet_text:  
+        regex_match = re.search("[A-Z][a-z]* [A-Z][a-z]*", tweet_text)
+        """TODO more regex match here"""
+    
+        if regex_match:
+            if "The " not in regex_match.group(0):
+                if regex_match.group(0) in host_dict.keys():
+                    host_dict[regex_match.group(0)] += 1
+                else:
+                    host_dict[regex_match.group(0)] = 1
+
+def getAwardWinner(tweet_text):
+    return
+
+def getpresenter(tweet_text):
+    return
+
+def getAttendance(tweet_text):
+    return
+
+def main():
+    all_tweets = pd.read_json("gg2018.json")
+    """loop into each line of tweet"""
+    for index, row in all_tweets.iterrows():
+        tweet_text = row["text"]
+        getHost(tweet_text)
+        """TODO more functions need to be called here"""
+        
+        
+    host = max(host_dict, key=host_dict.get)
+    print (host)
+    
+    
+    
+main()
+        
