fe71eee739d3f3987715d79e51c6e5b61c009de3
yannick-mamudo <yannick.mamudo@gmail.com>
2018-02-06 20:02:23 -0600

    Weird formatting issues, trying to consolidate awards clean up repetitions

--- host.py
+++ host.py
@@ -9,14 +9,14 @@ awards_dict = {}
 
 def wordLen(s):
     return len([i for i in s.split(' ') if i])
-	
-	
+
+
 def getHost(tweet_text):
     """TODO more phases here, not just only s monologue"""
-    if "s monologue" in tweet_text:  
+    if "s monologue" in tweet_text:
         regex_match = re.search("[A-Z][a-z]* [A-Z][a-z]*", tweet_text)
         """TODO more regex match here"""
-    
+
         if regex_match:
             if "The " not in regex_match.group(0):
                 if regex_match.group(0) in host_dict.keys():
@@ -24,28 +24,35 @@ def getHost(tweet_text):
                 else:
                     host_dict[regex_match.group(0)] = 1
 
+
 def getAwardWinner(tweet_text):
     return
+
 
 def getPresenter(tweet_text):
     return
 
+
 def getAttendance(tweet_text):
     return
 
+
 def getAwards(tweet_text):
     regex_match = re.search("\"Best [A-Za-z -]*\"", tweet_text)
-    
-    if regex_match:
-		if wordLen(regex_match.group(0)) <= 4：
-			return
-        if regex_match.group(0) in awards_dict.keys():
-            awards_dict[regex_match.group(0)] += 1
-        else:
-            awards_dict[regex_match.group(0)] = 1    
 
-			
-			
+    if regex_match:
+        # get award name and do some standardizing
+        award_name = regex_match.group(0)
+        award_name = re.sub(r'(television\s+series)|(tv\s+series)', 'TV Series', award_name, flags=re.IGNORECASE)
+        
+        if wordLen(award_name) <= 4:
+            return
+        if award_name in awards_dict.keys():
+            awards_dict[award_name] += 1
+        else:
+            awards_dict[award_name] = 1
+
+
 if __name__ == '__main__':
     all_tweets = pd.read_json("gg2018.json")
     """loop into each line of tweet"""
@@ -54,10 +61,9 @@ if __name__ == '__main__':
         getHost(tweet_text)
         getAwards(tweet_text)
         """TODO more functions need to be called here"""
-        
-        
+
     host = max(host_dict, key=host_dict.get)
-    print (host)
-	
+    print(host)
+
     for i in awards_dict:
         print(i)
